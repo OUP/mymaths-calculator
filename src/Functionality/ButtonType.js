42015b44b6a7e59f20f22e6e842d33a09990d033
Ryan Ehlers <ryan.ehlers@oup.com>
2018-02-06 21:45:06 +0000

    Refactor ButtonType.js and add function buttonSuperType

--- src/Functionality/ButtonType.js
+++ src/Functionality/ButtonType.js
@@ -1,10 +1,39 @@
 export function buttonType(b) {
-  if (b || b === 0) {
-    b = b.toString();
-  } else {
-    b = '';
-  }
+  b = safeToString(b);
 
+  switch (true) {
+    case checkForNumber(b):
+      return 'number';
+
+    case checkForOperator(b):
+      return 'operator';
+
+    case checkForMode(b):
+      return 'mode';
+
+    case checkForDisplay(b):
+      return 'display';
+
+    case checkForMisc(b):
+      return b;
+
+    case checkForNull(b):
+      return null;
+
+    default:
+      return 'function';
+  }
+}
+
+function safeToString(b) {
+  if (b || b === 0) {
+    return b.toString();
+  } else {
+    return '';
+  }
+}
+
+function checkForNumber(b) {
   if (
     b === parseFloat(b).toString() ||
     b.includes('.') ||
@@ -12,8 +41,14 @@ export function buttonType(b) {
     b === '(-)' ||
     b === '-'
   ) {
-    return 'number';
-  } else if (
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function checkForOperator(b) {
+  if (
     b === '+' ||
     b === '–' ||
     b === '×' ||
@@ -23,22 +58,58 @@ export function buttonType(b) {
     b === 'x²' ||
     b === 'x!'
   ) {
-    return 'operator';
-  } else if (
-    b === 'shift' ||
-    b === '⬅' ||
-    b === '➡' ||
-    b === '⬆' ||
-    b === '⬇'
-  ) {
-    return 'mode';
-  } else if (b === 'S⇔D' || b === 'ENG') {
-    return 'display';
-  } else if (b === 'DEL' || b === 'AC' || b === 'Ans' || b === '=') {
-    return b;
-  } else if (b === '') {
-    return null;
+    return true;
   } else {
-    return 'function';
+    return false;
+  }
+}
+
+function checkForMode(b) {
+  if (b === 'shift' || b === '⬅' || b === '➡' || b === '⬆' || b === '⬇') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function checkForDisplay(b) {
+  if (b === 'S⇔D' || b === 'ENG') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function checkForMisc(b) {
+  if (b === 'DEL' || b === 'AC' || b === 'Ans' || b === '=') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function checkForNull(b) {
+  if (b === '') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+export function buttonSuperType(button) {
+  const type = buttonType(button);
+
+  switch (true) {
+    case type === 'number' ||
+      type === 'operator' ||
+      type === 'function' ||
+      type === 'Ans':
+      return 'input';
+
+    case type === 'DEL' || type === 'AC':
+      return 'delete';
+
+    default:
+      return type;
   }
 }
