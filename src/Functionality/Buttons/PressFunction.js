7c848aa3042dbfe9b879004fa222530d465736b7
Ryan Ehlers <ryan.ehlers@oup.com>
2018-02-27 20:31:01 +0000

    Fix to x^n (still some bugs)

--- src/Functionality/Buttons/PressFunction.js
+++ src/Functionality/Buttons/PressFunction.js
@@ -1,3 +1,6 @@
+import { cloneState } from '../Utilities';
+import { pressMode } from './PressMode';
+
 export function pressFunction(button, currentState) {
   const fType = funcType(button);
 
@@ -68,41 +71,61 @@ function pressArgAfter(button, currentState) {
 function pressArgBothSides(button, currentState) {
   currentState.cursorPosition++;
   if (button === 'frac') {
-    currentState.functionKey++;
-    currentState.inputValue.push({
-      type: 'function',
-      function: 'numerator',
-      argument: [],
-      parts: 2,
-      key: currentState.functionKey
-      //Key used to destroy hidden characters that go with the function if the function is deleted
-    });
-    currentState.inputValue.push('cArg' + currentState.functionKey);
-    currentState.inputValue.push({
-      type: 'function',
-      function: 'denominator',
-      argument: [],
-      key: currentState.functionKey
-    });
-    currentState.inputValue.push('cArg' + currentState.functionKey);
+    pressFrac(currentState);
   } else if (button === 'xⁿ') {
-    currentState.inputValue.push({
-      type: 'function',
-      function: 'base',
-      argument: [],
-      parts: 2,
-      key: currentState.functionKey
-      //Key used to destroy hidden characters that go with the function if the function is deleted
-    });
-    currentState.functionKey++;
-    currentState.inputValue.push('cArg' + currentState.functionKey);
-    currentState.inputValue.push({
-      type: 'function',
-      function: 'exponent',
-      argument: [],
-      key: currentState.functionKey
-    });
-    currentState.inputValue.push('cArg' + currentState.functionKey);
+    pressPow(currentState);
   }
   return currentState;
+}
+
+function pressFrac(currentState) {
+  currentState.functionKey++;
+  currentState.inputValue.push({
+    type: 'function',
+    function: 'numerator',
+    argument: [],
+    parts: 2,
+    key: currentState.functionKey
+    //Key used to destroy hidden characters that go with the function if the function is deleted
+  });
+  currentState.inputValue.push('cArg' + currentState.functionKey);
+  currentState.inputValue.push({
+    type: 'function',
+    function: 'denominator',
+    argument: [],
+    key: currentState.functionKey
+  });
+  currentState.inputValue.push('cArg' + currentState.functionKey);
+  return;
+}
+
+function pressPow(currentState) {
+  let base;
+  if (currentState.inputValue.length) {
+    base = currentState.inputValue.pop();
+  }
+  currentState.inputValue.push({
+    type: 'function',
+    function: 'base',
+    argument: [],
+    parts: 2,
+    key: currentState.functionKey
+    //Key used to destroy hidden characters that go with the function if the function is deleted
+  });
+  if (base) {
+    currentState.inputValue.push(base);
+  }
+  currentState.functionKey++;
+  currentState.inputValue.push('cArg' + currentState.functionKey);
+  currentState.inputValue.push({
+    type: 'function',
+    function: 'exponent',
+    argument: [],
+    key: currentState.functionKey
+  });
+  currentState.inputValue.push('cArg' + currentState.functionKey);
+  if (base) {
+    currentState = pressMode('➡', currentState);
+  }
+  return;
 }
