28cfd4dc36739e3e81750904bb293bd1f7869ec7
Ryan Ehlers <ryan.ehlers@oup.com>
2018-07-04 17:08:17 +0100

    Tidy up symbol trig

--- src/Functionality/Eval/SymbolOps.js
+++ src/Functionality/Eval/SymbolOps.js
@@ -1,14 +1,14 @@
 import Decimal from 'decimal.js/decimal';
-const Fraction = require('fraction.js');
 import { FractionExpression, Term, Expression } from '../Classes/Symbol';
 import { numericOp } from './NumericOp';
-import { identicalArrays, checkIfFraction } from '../Utilities';
+import { checkIfFraction } from '../Utilities';
 import {
   SquareRoot,
   SqrtExpression,
   SqrtFractionExpression
 } from '../Classes/Surd';
 import processValue from './ProcessValue';
+import symbolicTrig from './SymbolicTrig';
 
 export function symbolicOp(v1, operation, v2 = 0) {
   v1 = construct(v1);
@@ -128,15 +128,15 @@ export function funcOnSymbol(func, arg, arg2) {
       break;
 
     case 'sin(x)':
-      result = symbolTrig('sin', arg);
+      result = symbolicTrig('sin', arg);
       break;
 
     case 'cos(x)':
-      result = symbolTrig('cos', arg);
+      result = symbolicTrig('cos', arg);
       break;
 
     case 'tan(x)':
-      result = symbolTrig('tan', arg);
+      result = symbolicTrig('tan', arg);
       break;
 
     case 'sin⁻¹':
@@ -173,290 +173,6 @@ function symbolPow(base, exponent) {
 
     case exponent < 0:
       return symbolPow(base, -exponent).reciprocal();
-  }
-}
-
-function symbolTrig(trigFunc, arg) {
-  if (isMultipleOfPi(arg)) {
-    return trigWithPi(trigFunc, arg);
-  } else {
-    return standardTrigFunc(trigFunc, arg.evaluate());
-  }
-}
-
-function isMultipleOfPi(arg) {
-  if (
-    numeratorIsMultipleOfPi(arg.numerator) &&
-    denominatorIsConstant(arg.denominator)
-  ) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-function numeratorIsMultipleOfPi(numerator) {
-  const terms = numerator.terms;
-  if (terms.length === 1) {
-    const powers = terms[0].powers;
-    const symbols = terms[0].symbols;
-    if (identicalArrays(symbols, ['π']) && powers[0].toString() === '1') {
-      return true;
-    }
-  }
-  return false;
-}
-
-function denominatorIsConstant(denominator) {
-  const terms = denominator.terms;
-  if (terms.length === 1) {
-    if (identicalArrays(terms[0].symbols, [])) {
-      return true;
-    }
-  }
-  return false;
-}
-
-function standardTrigFunc(trigFunc, arg) {
-  if (arg.constructor !== Decimal) {
-    arg = generateDecimal(arg);
-  }
-  switch (trigFunc) {
-    case 'sin':
-      return arg.sin();
-
-    case 'cos':
-      return arg.cos();
-
-    case 'tan':
-      return arg.tan();
-
-    default:
-      console.warn('invalid trigFunc');
-      return '0';
-  }
-}
-
-function generateDecimal(value) {
-  switch (value.constructor) {
-    case String:
-      return convertFracStringToDecimal(value);
-
-    case Fraction:
-      return convertFracStringToDecimal(value.toString());
-
-    case Decimal:
-      return value;
-
-    case Number:
-    default:
-      return new Decimal(value);
-  }
-}
-
-function trigWithPi(trigFunc, arg) {
-  const coefficient = setCoefficient(arg);
-
-  switch (trigFunc) {
-    case 'sin':
-      return sinWithPi(coefficient);
-
-    case 'cos':
-      return cosWithPi(coefficient);
-
-    case 'tan':
-      return tanWithPi(coefficient);
-
-    default:
-      console.warn('trig breakdown');
-      return 0;
-  }
-}
-
-function setCoefficient(arg) {
-  const coefficient = numericOp(
-    arg.numerator.terms[0].coefficient.toString(),
-    '÷',
-    arg.denominator.terms[0].coefficient.toString()
-  ).toString();
-  return new Fraction(coefficient).mod(2).toString();
-}
-
-function sinWithPi(coefStr) {
-  switch (coefStr) {
-    case '0':
-    case '1':
-    case '2':
-      return '0';
-
-    case '1/6':
-    case '0.1(6)':
-    case '5/6':
-    case '0.8(3)':
-      return '0.5';
-
-    case '0.25':
-    case '1/4':
-    case '0.75':
-    case '3/4':
-      return '√2/2';
-
-    case '0.(3)':
-    case '1/3':
-    case '2/3':
-    case '0.(6)':
-      return '√3/2';
-
-    case '0.5':
-    case '1/2':
-      return '1';
-
-    case '7/6':
-    case '1.1(6)':
-    case '11/6':
-    case '1.8(3)':
-      return '-0.5';
-
-    case '1.25':
-    case '5/4':
-    case '1.75':
-    case '7/4':
-      return '-√2/2';
-
-    case '1.(3)':
-    case '4/3':
-    case '5/3':
-    case '1.(6)':
-      return '-√3/2';
-
-    case '1.5':
-    case '3/2':
-      return '-1';
-
-    default:
-      const coef = convertFracStringToDecimal(coefStr);
-      return standardTrigFunc('sin', coef.times(Math.PI));
-  }
-}
-
-function cosWithPi(coefStr) {
-  switch (coefStr) {
-    case '0':
-    case '2':
-      return '1';
-
-    case '1/6':
-    case '0.1(6)':
-    case '11/6':
-    case '1.8(3)':
-      return '√3/2';
-
-    case '0.25':
-    case '1/4':
-    case '1.75':
-    case '7/4':
-      return '√2/2';
-
-    case '0.(3)':
-    case '1/3':
-    case '5/3':
-    case '1.(6)':
-      return '0.5';
-
-    case '0.5':
-    case '1/2':
-    case '1.5':
-    case '3/2':
-      return '0';
-
-    case '2/3':
-    case '0.(6)':
-    case '4/3':
-    case '1.(3)':
-      return '-0.5';
-
-    case '0.75':
-    case '3/4':
-    case '1.25':
-    case '5/4':
-      return '-√2/2';
-
-    case '0.8(3)':
-    case '5/6':
-    case '1.1(6)':
-    case '7/6':
-      return '-√3/2';
-
-    case '1':
-      return '-1';
-
-    default:
-      const coef = convertFracStringToDecimal(coefStr);
-      return standardTrigFunc('cos', coef.times(Math.PI));
-  }
-}
-
-function tanWithPi(coefStr) {
-  switch (coefStr) {
-    case '0':
-    case '1':
-    case '2':
-      return '0';
-
-    case '1/6':
-    case '0.1(6)':
-    case '7/6':
-    case '1.1(6)':
-      return '√3/3';
-
-    case '0.25':
-    case '1/4':
-    case '1.25':
-    case '5/4':
-      return '1';
-
-    case '0.(3)':
-    case '1/3':
-    case '4/3':
-    case '1.(3)':
-      return '√3';
-
-    case '0.5':
-    case '1/2':
-    case '1.5':
-    case '3/2':
-      return 'Maths error';
-
-    case '2/3':
-    case '0.(6)':
-    case '5/3':
-    case '1.(6)':
-      return '-√3';
-
-    case '0.75':
-    case '3/4':
-    case '1.75':
-    case '7/4':
-      return '-1';
-
-    case '0.8(3)':
-    case '5/6':
-    case '1.8(3)':
-    case '11/6':
-      return '-√3/3';
-
-    default:
-      const coef = convertFracStringToDecimal(coefStr);
-      return standardTrigFunc('tan', coef.times(Math.PI));
-  }
-}
-
-function convertFracStringToDecimal(fracString) {
-  if (checkIfFraction(fracString)) {
-    const fraction = new Fraction(fracString);
-    return new Decimal(fraction.n).div(fraction.d).times(fraction.s);
-  } else {
-    return new Decimal(fracString);
   }
 }
 
---
+++ src/Functionality/Eval/SymbolicTrig.js
@@ -0,0 +1,307 @@
+import Decimal from 'decimal.js/decimal';
+const Fraction = require('fraction.js');
+import { numericOp } from './NumericOp';
+import { identicalArrays, checkIfFraction } from '../Utilities';
+import {
+  SquareRoot,
+  SqrtExpression,
+  SqrtFractionExpression
+} from '../Classes/Surd';
+
+export default function symbolicTrig(trigFunc, arg) {
+  if (isMultipleOfPi(arg)) {
+    return trigWithPi(trigFunc, arg);
+  } else {
+    return standardTrigFunc(trigFunc, arg.evaluate());
+  }
+}
+
+function isMultipleOfPi(arg) {
+  if (
+    numeratorIsMultipleOfPi(arg.numerator) &&
+    denominatorIsConstant(arg.denominator)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function numeratorIsMultipleOfPi(numerator) {
+  const terms = numerator.terms;
+  if (terms.length === 1) {
+    const powers = terms[0].powers;
+    const symbols = terms[0].symbols;
+    if (identicalArrays(symbols, ['π']) && powers[0].toString() === '1') {
+      return true;
+    }
+  }
+  return false;
+}
+
+function denominatorIsConstant(denominator) {
+  const terms = denominator.terms;
+  if (terms.length === 1) {
+    if (identicalArrays(terms[0].symbols, [])) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function standardTrigFunc(trigFunc, arg) {
+  if (arg.constructor !== Decimal) {
+    arg = generateDecimal(arg);
+  }
+  switch (trigFunc) {
+    case 'sin':
+      return arg.sin();
+
+    case 'cos':
+      return arg.cos();
+
+    case 'tan':
+      return arg.tan();
+
+    default:
+      console.warn('invalid trigFunc');
+      return '0';
+  }
+}
+
+function generateDecimal(value) {
+  switch (value.constructor) {
+    case String:
+      return convertFracStringToDecimal(value);
+
+    case Fraction:
+      return convertFracStringToDecimal(value.toString());
+
+    case Decimal:
+      return value;
+
+    case Number:
+    default:
+      return new Decimal(value);
+  }
+}
+
+function trigWithPi(trigFunc, arg) {
+  const coefficient = setCoefficient(arg);
+  let result;
+
+  switch (trigFunc) {
+    case 'sin':
+      result = sinWithPi(coefficient);
+      break;
+
+    case 'cos':
+      result = cosWithPi(coefficient);
+      break;
+
+    case 'tan':
+      result = tanWithPi(coefficient);
+      break;
+
+    default:
+      console.warn('trig breakdown');
+      result = 0;
+      break;
+  }
+  return result.constructor === String ? result : sqrtFractionFactory(result);
+}
+
+function setCoefficient(arg) {
+  const coefficient = numericOp(
+    arg.numerator.terms[0].coefficient.toString(),
+    '÷',
+    arg.denominator.terms[0].coefficient.toString()
+  ).toString();
+  return new Fraction(coefficient).mod(2).toString();
+}
+
+function sinWithPi(coefStr) {
+  switch (coefStr) {
+    case '0':
+    case '1':
+    case '2':
+      return '0';
+
+    case '1/6':
+    case '0.1(6)':
+    case '5/6':
+    case '0.8(3)':
+      return '0.5';
+
+    case '0.25':
+    case '1/4':
+    case '0.75':
+    case '3/4':
+      return { numeratorString: '√2', denominatorString: '2' };
+
+    case '0.(3)':
+    case '1/3':
+    case '2/3':
+    case '0.(6)':
+      return '√3/2';
+
+    case '0.5':
+    case '1/2':
+      return '1';
+
+    case '7/6':
+    case '1.1(6)':
+    case '11/6':
+    case '1.8(3)':
+      return '-0.5';
+
+    case '1.25':
+    case '5/4':
+    case '1.75':
+    case '7/4':
+      return '-√2/2';
+
+    case '1.(3)':
+    case '4/3':
+    case '5/3':
+    case '1.(6)':
+      return '-√3/2';
+
+    case '1.5':
+    case '3/2':
+      return '-1';
+
+    default:
+      const coef = convertFracStringToDecimal(coefStr);
+      return standardTrigFunc('sin', coef.times(Math.PI));
+  }
+}
+
+function cosWithPi(coefStr) {
+  switch (coefStr) {
+    case '0':
+    case '2':
+      return '1';
+
+    case '1/6':
+    case '0.1(6)':
+    case '11/6':
+    case '1.8(3)':
+      return '√3/2';
+
+    case '0.25':
+    case '1/4':
+    case '1.75':
+    case '7/4':
+      return '√2/2';
+
+    case '0.(3)':
+    case '1/3':
+    case '5/3':
+    case '1.(6)':
+      return '0.5';
+
+    case '0.5':
+    case '1/2':
+    case '1.5':
+    case '3/2':
+      return '0';
+
+    case '2/3':
+    case '0.(6)':
+    case '4/3':
+    case '1.(3)':
+      return '-0.5';
+
+    case '0.75':
+    case '3/4':
+    case '1.25':
+    case '5/4':
+      return '-√2/2';
+
+    case '0.8(3)':
+    case '5/6':
+    case '1.1(6)':
+    case '7/6':
+      return '-√3/2';
+
+    case '1':
+      return '-1';
+
+    default:
+      const coef = convertFracStringToDecimal(coefStr);
+      return standardTrigFunc('cos', coef.times(Math.PI));
+  }
+}
+
+function tanWithPi(coefStr) {
+  switch (coefStr) {
+    case '0':
+    case '1':
+    case '2':
+      return '0';
+
+    case '1/6':
+    case '0.1(6)':
+    case '7/6':
+    case '1.1(6)':
+      return '√3/3';
+
+    case '0.25':
+    case '1/4':
+    case '1.25':
+    case '5/4':
+      return '1';
+
+    case '0.(3)':
+    case '1/3':
+    case '4/3':
+    case '1.(3)':
+      return '√3';
+
+    case '0.5':
+    case '1/2':
+    case '1.5':
+    case '3/2':
+      return 'Maths error';
+
+    case '2/3':
+    case '0.(6)':
+    case '5/3':
+    case '1.(6)':
+      return '-√3';
+
+    case '0.75':
+    case '3/4':
+    case '1.75':
+    case '7/4':
+      return '-1';
+
+    case '0.8(3)':
+    case '5/6':
+    case '1.8(3)':
+    case '11/6':
+      return '-√3/3';
+
+    default:
+      const coef = convertFracStringToDecimal(coefStr);
+      return standardTrigFunc('tan', coef.times(Math.PI));
+  }
+}
+
+function convertFracStringToDecimal(fracString) {
+  if (checkIfFraction(fracString)) {
+    const fraction = new Fraction(fracString);
+    return new Decimal(fraction.n).div(fraction.d).times(fraction.s);
+  } else {
+    return new Decimal(fracString);
+  }
+}
+
+function sqrtFractionFactory({ numeratorString, denominatorString }) {
+  const numeratorSqrt = new SqrtExpression([new SquareRoot(numeratorString)]);
+  const denominatorSqrt = new SqrtExpression([
+    new SquareRoot(denominatorString)
+  ]);
+  return new SqrtFractionExpression(numeratorSqrt, denominatorSqrt);
+}
