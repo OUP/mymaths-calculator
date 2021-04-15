aba4d2fe774017f79ab017b3466c94a81fa569f3
Ryan Ehlers <ryan.ehlers@oup.com>
2021-04-15 14:51:38 +0100

    Tidying

--- src/Functionality/Eval/DoArithmeticOp.js
+++ src/Functionality/Eval/DoArithmeticOp.js
@@ -15,41 +15,34 @@ export function doArithmeticOp(inputArray, position) {
 
 function evalArithmeticOp(inputArray, position, operation) {
   const valBefore = inputArray[position - 1].value;
-  let valAfter = '0';
-  if (inputArray[position + 1]) {
-    valAfter = inputArray[position + 1].value;
-  }
+  const valAfter = replaceUndefinedWithZero(inputArray[position + 1]);
   return opValue(valBefore, operation, valAfter);
 }
 
 function opValue(valBefore, operation, valAfter) {
-  if (checkForSymbols(valBefore) || checkForSymbols(valAfter)) {
-    return catchError(symbolicOp(valBefore, operation, valAfter));
-  } else {
-    return catchError(numericOp(valBefore, operation, valAfter));
-  }
+  return isSymbolOp(valBefore, valAfter)
+    ? symbolicOp(valBefore, operation, valAfter)
+    : numericOp(valBefore, operation, valAfter);
 }
 
 function updateArrayFromOp(inputArray, position, operation, output) {
-  if (operation !== 'x!' && operation !== '%') {
-    inputArray.splice(position - 1, 3, output);
-  } else {
-    inputArray.splice(position - 1, 2, output);
-  }
+  const numOfCharsInOp = isInfixOp(operation) ? 3 : 2;
+  inputArray.splice(position - 1, numOfCharsInOp, output);
 }
 
 function outputFactory(value) {
-  if (value.constructor === Decimal || value.constructor === Fraction) {
-    value = makeString(value);
-  }
-  const output = { value: value, type: buttonType(value) };
-  output.priority = opPriority(output);
-  return output;
+  const outputValue = isDecimalOrFraction(value) ? makeString(value) : value;
+  const output = { value: outputValue, type: buttonType(value) };
+  return Object.assign(output, { priority: opPriority(output) });
 }
 
-function catchError(value) {
-  if (value === 'NaN' || typeof value === 'undefined' || value === 'error') {
-    return ['error'];
-  }
-  return value;
-}
+const isSymbolOp = (valBefore, valAfter) =>
+  checkForSymbols(valBefore) || checkForSymbols(valAfter);
+
+const isInfixOp = operation => operation !== 'x!' && operation !== '%';
+
+const replaceUndefinedWithZero = val =>
+  typeof val !== 'undefined' ? val.value : '0';
+
+const isDecimalOrFraction = ({ constructor }) =>
+  constructor === Decimal || constructor === Fraction;
