561f6ccb59e5bf692dbe58ff767caec1c87f2ce2
Ryan Ehlers <ryan.ehlers@oup.com>
2021-04-16 16:21:55 +0100

    Can input negative symbols and square roots

--- src/Functionality/Eval/InsertImplicitOps/handleNegativesWithSymbols.js
+++ src/Functionality/Eval/InsertImplicitOps/handleNegativesWithSymbols.js
@@ -1,3 +1,27 @@
+import { checkForSymbols } from '../../Utilities';
+
 export default function handleNegativesWithSymbols(inputArray) {
+  for (let i = 0; i < inputArray.length - 1; i++) {
+    if (detectNegativeWithSymbol(inputArray, i)) {
+      inputArray = replaceSymbolWithInverse(inputArray, i);
+    }
+  }
+  return inputArray;
+}
+
+function detectNegativeWithSymbol(inputArray, i) {
+  return inputArray[i] === '-' && isSymbolElement(inputArray[i + 1]);
+}
+
+function isSymbolElement(el) {
+  return checkForSymbols(el) || isSqrt(el);
+}
+
+function isSqrt(el) {
+  return (el.function && el.function === '√(x)') || el === '√';
+}
+
+function replaceSymbolWithInverse(inputArray, i) {
+  inputArray.splice(i, 1, '-1', '×');
   return inputArray;
 }
