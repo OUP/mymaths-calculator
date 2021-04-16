37ce2b4f896c1ccb71818f28700e14b3adb5e029
Ryan Ehlers <ryan.ehlers@oup.com>
2021-04-16 18:16:44 +0100

    TeX buttons

--- src/UI/CalcButton.js
+++ src/UI/CalcButton.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import katex from 'katex';
 import PropTypes from 'prop-types';
 import buttonType from '../Functionality/ButtonType';
 import './CalcButton.css';
@@ -7,6 +8,13 @@ class CalcButton extends Component {
   constructor(props) {
     super(props);
     this.press = this.press.bind(this);
+  }
+
+  componentDidMount() {
+    const name = this.props.name;
+    if (hasTeX(name)) {
+      katex.render(nameToLabelMap(name), document.getElementById(name));
+    }
   }
 
   press() {
@@ -19,6 +27,7 @@ class CalcButton extends Component {
         <button
           className={buttonStyle(this.props.name, this.props.shift)}
           onClick={this.press}
+          id={this.props.name}
         >
           {nameToLabelMap(this.props.name)}
         </button>
@@ -74,12 +83,94 @@ function buttonStyle(button, shift = false) {
 
 function nameToLabelMap(name) {
   switch (name) {
+    case 'x²':
+      return '{x^2}';
+
+    case 'x³':
+      return '{x^3}';
+
+    case '|x|':
+      return '{\\text{Abs}}';
+
+    case 'xⁿ':
+      return '{x^{\\Box}}';
+
+    case 'x⁻¹':
+      return '{x^{-1}}';
+
+    case 'log(x)':
+      return '{\\text{log}}';
+
+    case 'logₐ(x)':
+      return '{\\text{log}_\\Box \\Box}';
+
+    case 'ln(x)':
+      return '{\\text{ln}}';
+
+    case 'eⁿ':
+      return 'e^{\\Box}';
+
+    case 'x!':
+      return '{x!}';
+
+    case 'frac':
+      return '{\\frac {\\Box} {\\Box}}';
+
+    case '√(x)':
+      return '{\\sqrt {\\Box}}';
+
+    case 'sin(x)':
+      return '{\\text{sin}}';
+
+    case 'sin⁻¹':
+      return '{\\text{sin}^{-1}}';
+
+    case 'cos(x)':
+      return '{\\text{cos}}';
+
+    case 'cos⁻¹':
+      return '{\\text{cos}^{-1}}';
+
+    case 'tan(x)':
+      return '{\\text{tan}}';
+
+    case 'tan⁻¹':
+      return '{\\text{tan}^{-1}}';
+
+    case '%':
+      return '{\\%}';
+
     case '-':
-      return '(-)';
+      return '{(\\text{-})}';
+
+    case '(':
+      return '{(}';
+
+    case ')':
+      return '{)}';
+
+    case 'π':
+      return '{π}';
+
+    // case '⬅':
+    //   return '{\\leftarrow}';
+
+    // case '➡':
+    //   return '{\\rightarrow}';
+
+    // case '⬆':
+    //   return '{\\uparrow}';
+
+    // case '⬇':
+    //   return '{\\downarrow}';
 
     default:
       return name;
   }
+}
+
+function hasTeX(name) {
+  return name.includes && nameToLabelMap(name).includes('{');
 }
 
 export default CalcButton;
