cd59b743ef1bb334de66a718ab07d10193f817c8
Ryan Ehlers <ryan.ehlers@oup.com>
2021-04-29 16:50:16 +0100

    Add square roots to special degree trig vals

--- src/Functionality/Eval/degTrig.js
+++ src/Functionality/Eval/degTrig.js
@@ -1,12 +1,14 @@
+import { sqrtFactory } from '../Classes/Surd';
+
 export default function degTrig(trigFunc, arg) {
   const floatArg = parseFloat(arg.toString());
-  return isMultipleOf15(floatArg)
+  return isSpecialValue(floatArg)
     ? specialValueTrig(trigFunc, floatArg)
     : standardTrigFunc(trigFunc, arg);
 }
 
-function isMultipleOf15(floatArg) {
-  return floatArg % 15 === 0;
+function isSpecialValue(floatArg) {
+  return floatArg % 30 === 0 || floatArg % 45 === 0;
 }
 
 function specialValueTrig(trigFunc, floatArg) {
@@ -37,11 +39,20 @@ function specialValueSin(floatArg) {
   switch (floatArg) {
     case 0:
     case 180:
+    case 360:
       return 0;
 
     case 30:
     case 150:
       return 0.5;
+
+    case 45:
+    case 135:
+      return sqrtFactory('√2').div(2);
+
+    case 60:
+    case 120:
+      return sqrtFactory('√3').div(2);
 
     case 90:
       return 1;
@@ -49,6 +60,14 @@ function specialValueSin(floatArg) {
     case 210:
     case 330:
       return -0.5;
+
+    case 225:
+    case 315:
+      return sqrtFactory('√2').div(-2);
+
+    case 240:
+    case 300:
+      return sqrtFactory('√3').div(-2);
 
     case 270:
       return -1;
@@ -61,6 +80,14 @@ function specialValueCos(floatArg) {
     case 360:
       return 1;
 
+    case 30:
+    case 150:
+      return sqrtFactory('√3').div(2);
+
+    case 45:
+    case 315:
+      return sqrtFactory('√2').div(2);
+
     case 60:
     case 300:
       return 0.5;
@@ -69,9 +96,17 @@ function specialValueCos(floatArg) {
     case 270:
       return 0;
 
-    case 210:
+    case 120:
     case 330:
       return -0.5;
+
+    case 135:
+    case 225:
+      return sqrtFactory('√2').div(-2);
+
+    case 150:
+    case 210:
+      return sqrtFactory('√3').div(-2);
 
     case 180:
       return -1;
@@ -85,17 +120,33 @@ function specialValueTan(floatArg) {
     case 360:
       return 0;
 
+    case 30:
+    case 210:
+      return sqrtFactory('√3').div(3);
+
     case 45:
     case 225:
       return 1;
+
+    case 60:
+    case 240:
+      return sqrtFactory('√3');
+
+    case 90:
+    case 270:
+      throw { name: 'Maths error' };
+
+    case 120:
+    case 300:
+      return sqrtFactory('√3').timesMinusOne();
 
     case 135:
     case 315:
       return -1;
 
-    case 90:
-    case 270:
-      throw { name: 'Maths error' };
+    case 150:
+    case 330:
+      return sqrtFactory('√3').div(-3);
   }
 }
 
